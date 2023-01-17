913a830cf6ad9b1c95e9dae178390f7b359bc02f
Covid In-Custody Project <117561631+covidincustody@users.noreply.github.com>
2023-01-17 16:00:17 -0500

    Update run.py

--- Code/run.py
+++ Code/run.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from utils import *
 
 covid = pd.read_csv("https://raw.githubusercontent.com/kangkai20000518/Covid_In-Custody_Project_Data_sourse/main/County%20Jails%20COVID%20Data%20Tracker%20-%20Population%20Tracker.csv",header=[2,3,4],encoding="ISO-8859-1",error_bad_lines=False)
