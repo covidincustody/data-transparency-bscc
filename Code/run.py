d5c31366d084384312ee98d9288f964aafb948fa
Covid In-Custody Project <117561631+covidincustody@users.noreply.github.com>
2023-03-23 01:04:43 -0700

    Update and rename data_transparency_run.py to run.py

--- Code/run.py
+++ Code/run.py
@@ -1,4 +1,4 @@
-from data_transparency_utils import *
+from utils import *
 
 #INPUT DATASET
 covid = pd.read_csv("https://raw.githubusercontent.com/kangkai20000518/Covid_In-Custody_Project_Data_sourse/main/County%20Jails%20COVID%20Data%20Tracker%20-%20Population%20Tracker.csv",header=[2,3,4],encoding="ISO-8859-1",error_bad_lines=False)
